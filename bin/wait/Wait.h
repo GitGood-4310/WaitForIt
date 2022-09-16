368cde78e51e10ce17894c61f90d91829e549063
alexish4 <alexish4@cpp.edu>
2022-09-16 15:34:30 -0700

    Working?

--- bin/wait/Wait.h
+++ bin/wait/Wait.h
@@ -1,6 +1,11 @@
 #ifndef __BIN_WAIT_WAIT_H
 #define __BIN_WAIT_WAIT_H
 
+#include <FreeNOS/User.h>
+#include "sys/wait.h"
+#include "sys/types.h"
+#include <errno.h>
+
 #include <POSIXApplication.h>
 
 /**
@@ -32,22 +37,4 @@ class Wait : public POSIXApplication
         virtual Result exec();
 };
 
-/**
- * @brief Wait for a child process to stop or terminate
- *
- * The wait() and waitpid() functions shall obtain status information
- * pertaining to one of the caller's child processes. Various options
- * permit status information to be obtained for child processes that
- * have terminated or stopped. If status information is available for
- * two or more child processes, the order in which their status is
- * reported is unspecified.
- *
- * @param pid Process ID of child to wait for.
- * @param stat_loc Points to an integer for storing the exit status.
- * @param options Optional flags.
- *
- * @return Process ID of the child on success or -1 on error
- */
-
-extern C pid_t waitpid(pid_t pid, int *stat_loc, int options);
 #endif
