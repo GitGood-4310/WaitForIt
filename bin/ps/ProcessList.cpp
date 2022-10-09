3a169c1ae325a3ce5b73173daa45a127293fdd61
alexish4 <alexish4@cpp.edu>
2022-10-09 02:46:11 +0000

    Submitting my work

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -26,37 +26,44 @@ ProcessList::ProcessList(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
     parser().setDescription("Output system process list");
+    parser().registerFlag('l', "priority", "display priority level of processes when you do");
 }
 
 ProcessList::Result ProcessList::exec()
 {
     const ProcessClient process;
     String out;
+    
+    if (!arguments().get("priority")) { //if no args
 
-    // Print header
-    out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+        // Print header
+        out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
 
-    // Loop processes
-    for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
-    {
-        ProcessClient::Info info;
-
-        const ProcessClient::Result result = process.processInfo(pid, info);
-        if (result == ProcessClient::Success)
+        // Loop processes
+        for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
         {
-            DEBUG("PID " << pid << " state = " << *info.textState);
+            ProcessClient::Info info;
 
-            // Output a line
-            char line[128];
-            snprintf(line, sizeof(line),
-                    "%3d %7d %4d %5d %10s %32s\r\n",
-                     pid, info.kernelState.parent,
-                     0, 0, *info.textState, *info.command);
-            out << line;
+            const ProcessClient::Result result = process.processInfo(pid, info);
+            if (result == ProcessClient::Success)
+            {
+                DEBUG("PID " << pid << " state = " << *info.textState);
+
+                // Output a line
+                char line[128];
+                snprintf(line, sizeof(line),
+                        "%3d %7d %4d %5d %10s %32s\r\n",
+                        pid, info.kernelState.parent,
+                        0, 0, *info.textState, *info.command);
+                out << line;
+            }
         }
-    }
 
-    // Output the table
-    write(1, *out, out.length());
-    return Success;
+        // Output the table
+        write(1, *out, out.length());
+        return Success;
+    }
+    else { //if using priority arg
+
+    }
 }
