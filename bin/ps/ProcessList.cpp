44953a384aee4315b2a8167a7e21cda7a6eb7b6f
alexish4 <alexish4@cpp.edu>
2022-10-09 05:08:38 +0000

    Made table look nicer

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -80,7 +80,7 @@ ProcessList::Result ProcessList::exec()
                 // Output a line
                 char line[128];
                 snprintf(line, sizeof(line),
-                        "%3d %7d %4d %5d %10s %9d %32s\r\n",
+                        "%3d %7d %4d %5d %10s %6d %32s\r\n",
                         pid, info.kernelState.parent,
                         0, 0, *info.textState, info.priorityLevel, *info.command);
                 out << line;
